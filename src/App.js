58a5bd716d5f35b0d9b6d02a01dbac81a8f5cd6e
Tausif4171 <tausifkhan4171@gmail.com>
2022-12-12 23:59:06 +0530

    navbar component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <h2>Notes App</h2>
+      <Navbar />
     </div>
   );
 }
