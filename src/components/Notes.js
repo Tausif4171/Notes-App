5473eadd0c345155a59e244a63705e00b0062e74
Tausif4171 <tausifkhan4171@gmail.com>
2023-01-02 23:26:39 +0530

    error fixed

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -9,42 +9,38 @@ import Container from "@mui/material/Container";
 export default function Notes({ handleClickOpen, title, desc, notes }) {
   return (
     <Container maxWidth="xs">
-      {notes.map((notes) => {
-        return (
-          <div style={{}}>
-            <h2>Your Notes</h2>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardContent>
-                <Typography variant="h5" component="div">
-                  Title
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {notes.title}
-                </Typography>
-              </CardContent>
-              <CardContent>
-                <Typography variant="h5" component="div">
-                  Description
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {notes.desc}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button
-                  variant="outlined"
-                  color="primary"
-                  onClick={handleClickOpen}
-                  size="small"
-                >
-                  Edit
-                </Button>
-                <Button size="small">Delete</Button>
-              </CardActions>
-            </Card>
-          </div>
-        );
-      })}
+      <div style={{}}>
+        <h2>Your Notes</h2>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardContent>
+            <Typography variant="h5" component="div">
+              Title
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {/* {notes.title} */}
+            </Typography>
+          </CardContent>
+          <CardContent>
+            <Typography variant="h5" component="div">
+              Description
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {/* {notes.desc} */}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={handleClickOpen}
+              size="small"
+            >
+              Edit
+            </Button>
+            <Button size="small">Delete</Button>
+          </CardActions>
+        </Card>
+      </div>
     </Container>
   );
 }
